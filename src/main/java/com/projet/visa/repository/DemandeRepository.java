7d2a02adf419e433f967153c0e8597f23db44090
Tandrifiniaina Dylan RAJAONARIVONY <tandrifudylan@gmail.com>
2026-04-21 19:57:23 +0300

    declaration des methodes demande findAll & findById

--- src/main/java/com/projet/visa/repository/DemandeRepository.java
+++ src/main/java/com/projet/visa/repository/DemandeRepository.java
@@ -2,7 +2,10 @@ package com.projet.visa.repository;
 
 import com.projet.visa.model.Demande;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
 public interface DemandeRepository extends JpaRepository<Demande, Integer> {
-
+    public List<Demande> findAll();
+    public Optional<Demande> findById(Integer id);
 }
