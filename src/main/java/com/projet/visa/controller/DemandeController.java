76e7bd914fb38684227ec22b82cec714c4e5e814
Tandrifiniaina Dylan RAJAONARIVONY <tandrifudylan@gmail.com>
2026-04-21 20:13:13 +0300

    implimentation filtre -> controller

--- src/main/java/com/projet/visa/controller/DemandeController.java
+++ src/main/java/com/projet/visa/controller/DemandeController.java
@@ -1,14 +1,21 @@
 package com.projet.visa.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.projet.visa.model.Demande;
+import com.projet.visa.model.Visa;
+import com.projet.visa.model.VisaType;
 import com.projet.visa.repository.DemandeRepository;
+import com.projet.visa.repository.DemandeTypeRepository;
+import com.projet.visa.repository.VisaTypeRepository;
 
 
 @Controller
@@ -16,15 +23,36 @@ import com.projet.visa.repository.DemandeRepository;
 public class DemandeController {
 
     private final DemandeRepository demandeRepository;
+    private final DemandeTypeRepository demandeTypeRepository;
+    private final VisaTypeRepository visaTypeRepository;
 
-    public DemandeController(DemandeRepository demandeRepository) {
+    public DemandeController(DemandeRepository demandeRepository, DemandeTypeRepository demandeTypeRepository, VisaTypeRepository visaTypeRepository) {
         this.demandeRepository = demandeRepository;
+        this.demandeTypeRepository = demandeTypeRepository;
+        this.visaTypeRepository = visaTypeRepository;
     }
 
     @GetMapping("/liste")
-    public String getListeDemande(Model model) {
-        List<Demande> demandes = demandeRepository.findAll();
+    public String getListeDemande(
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateMin,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateMax,
+            @RequestParam(required = false) Integer typeId,
+            @RequestParam(required = false) Integer visaTypeId,
+        Model model) {
+
+        List<Demande> demandes = demandeRepository.search(dateMin, dateMax, typeId, visaTypeId);
+
         model.addAttribute("demandes", demandes);
+        model.addAttribute("demandeTypes", demandeTypeRepository.findAll());
+        model.addAttribute("visaTypes", visaTypeRepository.findAll());
+
+        model.addAttribute("dateMin", dateMin);
+        model.addAttribute("dateMax", dateMax);
+        model.addAttribute("typeId", typeId);
+        model.addAttribute("visaTypeId", visaTypeId);
+
 
         return "demande/liste";
     }
