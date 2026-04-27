5eff08002199b3621a9b1f1f9a149e9dedb5947e
Gaellane <gaellaneandriantsoa37@gmail.com>
2026-04-27 14:20:46 +0300

    Update DemandeurController.java

--- src/main/java/com/projet/visa/controller/DemandeurController.java
+++ src/main/java/com/projet/visa/controller/DemandeurController.java
@@ -29,7 +29,7 @@ public class DemandeurController {
         this.passeportService=passeportService;
     }
 
-    @GetMapping("")
+    @GetMapping
     public String list(Model model) {
         model.addAttribute("demandeurs", demandeurService.findAll());
         return "demandeur/list";
@@ -51,7 +51,7 @@ public class DemandeurController {
         return "demandeur/form";
     }
 
-    @PostMapping("/")
+    @PostMapping
     public String create(
             @RequestParam String nom,
             @RequestParam(required = false) String prenom,
